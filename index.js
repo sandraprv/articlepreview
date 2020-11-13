46ec8b9ff5d13f2998a71e5a20bd2ab077c4622c
Sandra Périvier <sandraperivier@MacBook-Air-de-Sandra.home>
2020-11-13 12:02:57 +0100

    add js and update css

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let btn = document.getElementById("share-btn");
+let icons = document.getElementById("icons");
+
+btn.addEventListener("click", (event) => {
+  icons.classList.toggle("is-visible");
+});
